c4e286cfa454775171e0f93b99ec20b659e91964
Yume24 <antonijaros04@gmail.com>
2025-07-10 13:24:23 +0200

    lang prop fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children, }: Readonly<{ children: React.ReactNode; }>) {
   return (
-    <html lang="en" data-theme="light">
+    <html lang="pl" data-theme="light">
       <body
         className={'antialiased'}>
         <Navbar />
