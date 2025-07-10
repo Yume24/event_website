5b0a8617f00e9d73571934c1c20938d430f77765
Yume24 <antonijaros04@gmail.com>
2025-07-10 19:18:35 +0200

    carousel style and photos

--- src/components/carousel/Carousel.tsx
+++ src/components/carousel/Carousel.tsx
@@ -11,8 +11,14 @@ type Image = {
 }
 const images: Image[] = [
     {src: "/static/images/carousel/slideshow1.jpg", width: 4080, height: 2296 },
-    {src: "/static/images/carousel/slideshow2.jpg", width: 4080, height: 2296 },
-    {src: "/static/images/carousel/slideshow3.jpg", width: 4080, height: 2296 },
+    {src: "/static/images/carousel/slideshow2.jpg", width: 2048, height: 1536 },
+    {src: "/static/images/carousel/slideshow3.jpg", width: 2048, height: 1536 },
+    {src: "/static/images/carousel/slideshow4.jpg", width: 4080, height: 2296 },
+    {src: "/static/images/carousel/slideshow5.jpg", width: 4080, height: 2296 },
+    {src: "/static/images/carousel/slideshow6.jpg", width: 2048, height: 1365 },
+    {src: "/static/images/carousel/slideshow7.jpg", width: 1620, height: 1080 },
+    {src: "/static/images/carousel/slideshow8.jpg", width: 1599, height: 1066 },
+    {src: "/static/images/carousel/slideshow9.jpg", width: 2048, height: 1148 },
 ];
 
 export default function Carousel() {
@@ -28,7 +34,7 @@ export default function Carousel() {
             }
         };
 
-        const interval = setInterval(() => next(), 5000);
+        const interval = setInterval(() => next(), 3000);
         return () => {
             clearInterval(interval);
         }
@@ -39,9 +45,9 @@ export default function Carousel() {
             <AnimatePresence mode="popLayout">
                 <motion.div
                 className="w-full h-full"
-                initial={{ x: 1250 }}
-                animate={{ x: 0 }}
-                exit={{ opacity: 0, x: -1250 }}
+                initial={{ x: 1725, }}
+                animate={{ x: 0, }}
+                exit={{ x: -1725, }}
                 key={images[currentSlide].src}
                 transition={{ duration: 1 }}
                 >
