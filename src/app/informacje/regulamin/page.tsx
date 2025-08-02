80ba93fd1f8128c8ad1718d6722e79824cedaf9e
Yume24 <antonijaros04@gmail.com>
2025-08-02 17:01:35 +0200

    add regulaminy

---
+++ src/app/informacje/regulamin/page.tsx
@@ -0,0 +1,4 @@
+export default function InformacjeRegulamin() {
+    return <iframe src="/static/documents/regulamin.pdf" className="m-auto h-screen"
+                   width="100%"/>
+}
---
+++ src/app/konkurs/regulamin/page.tsx
@@ -0,0 +1,4 @@
+export default function KonkursRegulamin() {
+    return <iframe src="/static/documents/regulamin_GT.pdf" className="m-auto h-screen"
+                   width="100%"/>
+}
