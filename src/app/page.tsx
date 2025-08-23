6387f462c963d941110b4bbf2f0c07ee64e8c528
Yume24 <antonijaros04@gmail.com>
2025-08-23 12:45:52 +0200

    add patrons section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Hero from "@/components/hero/Hero";
 import Countdown from "@/components/countdown/Countdown";
 import Sponsors from "@/components/sponsors/Sponsors";
+import Patrons from "@/components/sponsors/Patrons";
 
 export default function Home() {
     return (
@@ -16,8 +17,10 @@ export default function Home() {
                     <h2 className="text-4xl m-10 text-center">Nasi Sponsorzy:</h2>
                     <Sponsors/>
                 </section>
+                <div className="divider divider-neutral opacity-50 w-1/2 m-auto"></div>
                 <section>
                     <h2 className="text-4xl m-10 text-center">Patroni Medialni:</h2>
+                    <Patrons/>
                 </section>
             </main>
         </>
