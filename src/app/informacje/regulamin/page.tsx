e8105cd08d1b2b87d77d1f5a1c815e719dcb1151
Yume24 <antonijaros04@gmail.com>
2025-08-23 12:45:00 +0200

    change display logic for pdfs

--- src/app/informacje/regulamin/page.tsx
+++
@@ -1,4 +0,0 @@
-export default function InformacjeRegulamin() {
-    return <iframe src="/static/documents/regulamin.pdf" className="m-auto h-screen"
-                   width="100%"/>
-}
--- src/app/konkurs/regulamin/page.tsx
+++
@@ -1,4 +0,0 @@
-export default function KonkursRegulamin() {
-    return <iframe src="/static/documents/regulamin_GT.pdf" className="m-auto h-screen"
-                   width="100%"/>
-}
