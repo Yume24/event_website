75e8a4b5c050767ac1a00250c12c0e5d7a050c54
Yume24 <antonijaros04@gmail.com>
2025-11-16 17:15:51 +0100

    change menu links order

--- src/components/navbar/MenuLinks.ts
+++ src/components/navbar/MenuLinks.ts
@@ -10,8 +10,8 @@ export const menuLinks: MenuLink[] = [
       { text: 'Regulamin', href: '/regulamin' },
       { text: 'Klasy startowe', href: '/klasy-startowe' },
       { text: 'Harmonogram wystawy', href: '/harmonogram' },
-      { text: 'Kalendarz modelarski', href: '/kalendarz' },
-      { text: 'Wyniki', href: '/wyniki' }
+      { text: 'Wyniki', href: '/wyniki' },
+      { text: 'Kalendarz modelarski', href: '/kalendarz' }
     ]
   },
   {
