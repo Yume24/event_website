7a3ac9e8028902d5cd49c893659ea8def33f0b2c
Yume24 <antonijaros04@gmail.com>
2025-12-07 17:02:24 +0100

    replace GT-Cup exhibitions list with embedded calendar PDF

--- src/app/konkurs/wystawy/page.tsx
+++ src/app/konkurs/wystawy/page.tsx
@@ -1,49 +1,8 @@
 export default function GTCupWystawy() {
   return (
-    <main className="min-h-screen bg-base-100 text-base-content py-16 px-6 md:px-12">
-      <section className="max-w-3xl mx-auto">
-        <h1 className="text-3xl md:text-4xl font-bold text-primary border-b-2 border-primary pb-2 mb-8">
-          Wystawy w PL zaliczane do kategorii GT-Cup
-        </h1>
-
-        <ul className="list-decimal list-inside space-y-2">
-          <li>
-            <b>Meeting Modelarski Ogrodzieniec 2026</b> (11.04.2026 r.)
-          </li>
-          <li>
-            <b>Ogólnopolski Konkurs Modeli Redukcyjnych Strzelec 2026</b>{' '}
-            (18-19.04.2026)
-          </li>
-          <li>
-            <b>Rybnicki Festiwal Modelarski HOYM MODEL FEST 2026</b>{' '}
-            (09-10.05.2026)
-          </li>
-          <li>
-            <b>Inowrocławski Konkurs Modelarski 2026</b> (20-21.06.2026)
-          </li>
-          <li>
-            <b>
-              Międzynarodowy Konkurs Modeli Kartonowych i Plastikowych Radków
-              2026
-            </b>{' '}
-            (26-28.06.2026)
-          </li>
-          <li>
-            <b>XV Bałtycki Festiwal Modelarski</b> (03-05.07.2026)
-          </li>
-          <li>
-            <b>XVII Festiwal Modelarski Jaworzno 2026</b> (12-13.09.2026)
-          </li>
-          <li>
-            <b>XIV Szczeciński Festiwal Modelarski Paprykarz 2026</b>{' '}
-            (03-04.10.2026 r.)
-          </li>
-          <li>
-            <b>XXV Konkurs Modeli Redukcyjnych Łambinowice 2026</b>{' '}
-            (06-08.11.2026)
-          </li>
-        </ul>
-      </section>
-    </main>
+    <iframe
+      className="w-full h-[calc(100dvh-10.75rem)]"
+      src="/konkurs/kalendarz_GT_CUP.pdf"
+    />
   );
 }
